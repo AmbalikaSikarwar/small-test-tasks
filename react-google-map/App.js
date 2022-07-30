a6d8bc8f3792f27d6c80bad51f1c00d5d7b6a195
AmbalikaSikarwar <105535702+AmbalikaSikarwar@users.noreply.github.com>
2022-07-30 15:13:15 +0530

    Create App.js

---
+++ react-google-map/App.js
@@ -0,0 +1,19 @@
+import Rect from "react";
+import Googlemap from "./components/Googlemap"
+
+function App() {
+
+
+
+    return(
+        <div>
+        <center>
+        <h6>google Maps Integration</h6>
+        <Googlemap />
+        </center>
+
+        </div>
+    )
+}
+
+export default App;
