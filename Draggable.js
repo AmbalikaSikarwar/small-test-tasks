be0f6e9a0e6c8691af1db5a573f08c51367e5ce8
AmbalikaSikarwar <105535702+AmbalikaSikarwar@users.noreply.github.com>
2022-08-22 15:36:24 +0530

    Update Draggable.js

--- Draggable.js
+++ Draggable.js
@@ -2,17 +2,42 @@ import React, { useState, useRef } from 'react';
 import './App.css';
  
 const App = () => {
+  
+  const dragItem = useRef();
+  const dragOverItem = useRef();
+  const [list, setList] = useState(['Ambalika','Mahima','Madhu','Santoshi','Ruchika']);
  
-  const [list, setList] = useState(['Ambalika','Mahima','Santoshi','Ruchika','Madhu']);
+  const dragStart = (e, position) => {
+    dragItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+ 
+  const dragEnter = (e, position) => {
+    dragOverItem.current = position;
+    console.log(e.target.innerHTML);
+  };
+ 
+  const drop = (e) => {
+    const copyListItems = [...list];
+    const dragItemContent = copyListItems[dragItem.current];
+    copyListItems.splice(dragItem.current, 1);
+    copyListItems.splice(dragOverItem.current, 0, dragItemContent);
+    dragItem.current = null;
+    dragOverItem.current = null;
+    setList(copyListItems);
+  };
  
   return (
     <>
     {
     list&&
     list.map((item, index) => (
-      <div style={{backgroundColor:'pink', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+      <div style={{backgroundColor:'grey', margin:'20px 25%', textAlign:'center', fontSize:'40px'}}
+        onDragStart={(e) => dragStart(e, index)}
+        onDragEnter={(e) => dragEnter(e, index)}
+        onDragEnd={drop}
         key={index}
-      draggable>
+        draggable>
           {item}
       </div>
       ))}
